9c99478fb34bca03809626d3e9038490c24d446f
Zerplin <43621940+Zerplin@users.noreply.github.com>
2023-02-27 10:57:51 +0100

    modify generator function to return pandas dataframe

---
+++ Dataset_Generator/DatasetGenerator.py
@@ -0,0 +1,43 @@
+# from pypuf.simulation import ArbiterPUF
+import numpy as np
+from pypuf.simulation import ArbiterPUF
+from pypuf.io import random_inputs
+import pandas as pd
+
+
+class DatasetGenerator:
+    """
+    returns an arbiter puf
+    """
+
+    @staticmethod
+    def _get_arbiter_puf(input_dim, seed):
+        return ArbiterPUF(n=input_dim, seed=seed)
+
+    """
+    returns an array of challenges
+    """
+
+    @staticmethod
+    def _get_challenges(input_dim, number_of_challenges, seed):
+        return random_inputs(n=input_dim, N=number_of_challenges, seed=seed)
+
+    @staticmethod
+    def _get_challenge_response_pairs(puf, challenges):
+        responses = puf.eval(challenges)
+        challenge_response_pairs = []
+
+        for idx, response in np.ndenumerate(responses):
+            challenge_response_list = np.array(challenges[idx].tolist() + [response])
+            print(challenge_response_list)
+            challenge_response_pairs.append(challenge_response_list)
+        challenge_response_pairs_df = pd.DataFrame(np.row_stack(challenge_response_pairs))
+        return challenge_response_pairs_df
+    """
+    returns a pandas dataframe, last element is the response, rest is the challenge bits
+    """
+    @staticmethod
+    def get_arbiter_dataset(input_dim=2, puf_seed=1337, challenge_seed=1337, number_of_challenges=3):
+        puf = DatasetGenerator._get_arbiter_puf(input_dim, puf_seed)
+        challenges = DatasetGenerator._get_challenges(input_dim, number_of_challenges, challenge_seed)
+        return DatasetGenerator._get_challenge_response_pairs(puf, challenges)
---
+++ main.py
@@ -0,0 +1,4 @@
+from Dataset_Generator.DatasetGenerator import DatasetGenerator
+
+print(DatasetGenerator.get_arbiter_dataset())
+
