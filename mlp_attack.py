8dee7bd28f75400fad1b7f3cdef2a8bb6b1dcfe1
Jupiops <github@jupiops.net>
2023-05-01 15:37:26 +0200

    mlp fix time measurement and plot learning curve ✨

--- mlp_attack.py
+++ mlp_attack.py
@@ -2,11 +2,12 @@ import time
 
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 
-data = 'challenge_response100k.csv'
+data = 'challenge_response10k.csv'
 
 
 def mlp_attack():
@@ -30,10 +31,17 @@ def mlp_attack():
     print(X_train.shape, X_test.shape)
     print(y_train.shape, y_test.shape)
 
+    clf_mlp = MLPClassifier( learning_rate_init=0.01, random_state=0, early_stopping=False)
     start = time.time()
-    clf_mlp = MLPClassifier(random_state=0, early_stopping=False)
     clf_mlp.fit(X_train, y_train)
     elapsed = time.time() - start
+
+    # Plot the LEARNING CURVE
+    plt.title("Evolution of TRAINING ERROR during training")
+    plt.xlabel("Iterations (epochs)")
+    plt.ylabel("TRAINING ERROR")
+    plt.plot(clf_mlp.loss_curve_)
+    plt.show()
 
     y_pred = clf_mlp.predict(X_test)
 
